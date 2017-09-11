4ced794bc7bcc7e078f1e3e041fda3a8c35e7a0a
Yicen Shi <shiy918@newschool.edu>
2017-09-11 14:58:15 +0000

    Street Addresses

---
+++ meet2.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var cheerio = require('cheerio');
+
+// load the text into content
+var content = fs.readFileSync('m08.txt');
+
+// load `content` into a cheerio object
+var $ = cheerio.load(content);
+
+// select the corresponding element and print out the address and trim
+$('td').each(function(i, elem) {
+    if ($(elem).attr('style')=="border-bottom:1px solid #e3e3e3; width:260px") {
+        console.log($(elem).contents().get(6).nodeValue.trim())
+    } 
+});
