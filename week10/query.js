e00934d82987b5fd4e6454e8c55c19a36574705d
Yicen Shi <shiy918@newschool.edu>
2017-11-04 01:21:16 +0000

    week10 code

---
+++ week10/query.js
@@ -0,0 +1,35 @@
+var express = require('express'),
+    app = express();
+const { Pool } = require('pg');
+
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'shiy918';
+db_credentials.host = process.env.AWSRDS_EP;
+db_credentials.database = 'shidb';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+
+app.get('/', function(req, res) {
+    // Connect to the AWS RDS Postgres database
+    const client = new Pool(db_credentials);
+
+    // SQL query
+    var q = `SELECT EXTRACT(DAY FROM sensortime AT TIME ZONE 'America/New_York') as sensorday, 
+             EXTRACT(MONTH FROM sensortime AT TIME ZONE 'America/New_York') as sensormonth, 
+             lightsensor, 
+             magnetsensor,
+             FROM sensorData,
+             GROUP BY sensormonth, sensorday;`;
+             
+    client.connect();
+    client.query(q, (qerr, qres) => {
+        res.send(qres.rows);
+        console.log('responded to request');
+    });
+    client.end();
+});
+
+app.listen(3000, function() {
+    console.log('Server listening...');
+});
